024d8f0582dddf3a6d0bbe06d339b9ddec4d1a75
Malan-Tai <malantai99@gmail.com>
2021-07-12 11:56:10 +0200

    Readd UI

---
+++ ECS MatterVRX/Assets/Scripts/MonoBehaviours/VRInputModule.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Valve.VR;
+
+public class VRInputModule : BaseInputModule
+{
+    public Camera cam;
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Action_Boolean click;
+
+    public GameObject indicator;
+
+    private GameObject curObject = null;
+    private PointerEventData data = null;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        data = new PointerEventData(eventSystem);
+    }
+
+    public override void Process()
+    {
+        data.Reset();
+        data.position = new Vector2(cam.pixelWidth / 2, cam.pixelHeight / 2);
+
+        eventSystem.RaycastAll(data, m_RaycastResultCache);
+        data.pointerCurrentRaycast = FindFirstRaycast(m_RaycastResultCache);
+        curObject = data.pointerCurrentRaycast.gameObject;
+
+        indicator.transform.position = data.pointerCurrentRaycast.worldPosition;
+
+        m_RaycastResultCache.Clear();
+
+        HandlePointerExitAndEnter(data, curObject);
+
+        if (click.GetStateDown(handType)) ProcessPress(data);
+
+        if (click.GetStateUp(handType)) ProcessRelease(data);
+    }
+
+    private void ProcessPress(PointerEventData d)
+    {
+        data.pointerPressRaycast = data.pointerCurrentRaycast;
+
+        GameObject newPointerPress = ExecuteEvents.ExecuteHierarchy(curObject, d, ExecuteEvents.pointerDownHandler);
+
+        if (newPointerPress == null) newPointerPress = ExecuteEvents.GetEventHandler<IPointerClickHandler>(curObject);
+
+        d.pressPosition = d.position;
+        d.pointerPress = newPointerPress;
+        d.rawPointerPress = curObject;
+    }
+
+    private void ProcessRelease(PointerEventData d)
+    {
+        ExecuteEvents.Execute(d.pointerPress, d, ExecuteEvents.pointerUpHandler);
+
+        GameObject pointerUpHandler = ExecuteEvents.GetEventHandler<IPointerClickHandler>(curObject);
+
+        if (d.pointerPress == pointerUpHandler)
+        {
+            ExecuteEvents.Execute(d.pointerPress, d, ExecuteEvents.pointerClickHandler);
+        }
+
+        eventSystem.SetSelectedGameObject(null);
+
+        d.pressPosition = Vector2.zero;
+        d.pointerPress = null;
+        d.rawPointerPress = null;
+    }
+}
