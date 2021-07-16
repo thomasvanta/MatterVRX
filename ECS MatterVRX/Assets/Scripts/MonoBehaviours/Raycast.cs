0b6fccd5aae877369caa9503b2f471eb9faadfef
Malan-Tai <malantai99@gmail.com>
2021-07-16 11:23:33 +0200

    added laser pointer

--- ECS MatterVRX/Assets/Scripts/MonoBehaviours/Raycast.cs
+++ ECS MatterVRX/Assets/Scripts/MonoBehaviours/Raycast.cs
@@ -5,16 +5,20 @@ using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Physics;
 using Unity.Physics.Systems;
+using Unity.Transforms;
 using Valve.VR;
 
 public class Raycast : MonoBehaviour
 {
     [SerializeField] private SteamVR_Behaviour_Pose pose;
     [SerializeField] private SteamVR_Input_Sources handType;
+    [SerializeField] private GameObject laserPrefab;
+    private GameObject laser;
+    private Transform laserTransform;
 
     private Entity lastEntity = Entity.Null;
     private EntityManager entityManager;
-    private Entity DoRaycast(float3 from, float3 to)
+    private Entity DoRaycast(float3 from, float3 to, out float3 hitPos)
     {
         BuildPhysicsWorld buildPhysicsWorld = World.DefaultGameObjectInjectionWorld.GetExistingSystem<BuildPhysicsWorld>();
         CollisionWorld collisionWorld = buildPhysicsWorld.PhysicsWorld.CollisionWorld;
@@ -35,11 +39,13 @@ public class Raycast : MonoBehaviour
 
         if (collisionWorld.CastRay(raycastInput, out raycastHit))
         {
+            hitPos = raycastHit.Position;
             Entity hitEntity = buildPhysicsWorld.PhysicsWorld.Bodies[raycastHit.RigidBodyIndex].Entity;
             return hitEntity;
         }
         else
         {
+            hitPos = float3.zero;
             return Entity.Null;
         }
     }
@@ -47,18 +53,24 @@ public class Raycast : MonoBehaviour
     private void Start()
     {
         entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+        laser = Instantiate(laserPrefab);
+        laserTransform = laser.transform;
     }
 
     private void Update()
     {
-        Entity hit = DoRaycast(pose.transform.position, 100f * pose.transform.forward + pose.transform.position);
+        float3 hitPos;
+        Entity hit = DoRaycast(pose.transform.position, 100f * pose.transform.forward + pose.transform.position, out hitPos);
 
         if (hit == Entity.Null && lastEntity != Entity.Null)
         {
             entityManager.SetComponentData(lastEntity, new OutlineComponent { isSelected = false });
             lastEntity = Entity.Null;
+
+            laser.SetActive(false);
         }
-        else if (hit != Entity.Null && lastEntity != hit)
+        else if (hit != Entity.Null)
         {
             if (lastEntity != Entity.Null)
             {
@@ -66,6 +78,16 @@ public class Raycast : MonoBehaviour
             }
             entityManager.SetComponentData(hit, new OutlineComponent { isSelected = true, color = new float4(1, 1, 1, 1) });
             lastEntity = hit;
+
+            ShowLaser(hitPos);
         }
+    }
+
+    private void ShowLaser(float3 hitPos)
+    {
+        laser.SetActive(true);
+        laserTransform.position = Vector3.Lerp(pose.transform.position, hitPos, .5f);
+        laserTransform.LookAt(hitPos);
+        laserTransform.localScale = new Vector3(laserTransform.localScale.x, laserTransform.localScale.y, math.distance(hitPos, pose.transform.position));
     }
 }
